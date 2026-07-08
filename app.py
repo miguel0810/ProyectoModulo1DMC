ebc7e07e648d8a88d9abf691d249af5d5c842c1a
Miguel Flores Ccama <mif_112@hotmail.com>
2026-07-07 22:29:41 -0500

    Update app.py

---
+++ app.py
@@ -0,0 +1,10 @@
+import streamlit as st
+st.title("Titulo")
+st.sidebar.title("Parametros")
+
+valor_inicial = st.number_input("ingrese valor i", value = 0)
+valor_final = st.number_input("sasdad", value = 1)
+
+
+lista_numerica = list(range(valor_inicial,valor_final)
+st.write(lista_numerica)
