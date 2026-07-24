101ca67040459b5f7536f804b44ef459d892667a
Miguel Flores Ccama <mif_112@hotmail.com>
2026-07-23 19:29:57 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import streamlit as st
-st.title("Titulo")
+st.title("Proyecto modulo 1 fundamentals")
 st.sidebar.title("Parametros")
 
 valor_inicial = st.number_input("ingrese valor i", value = 0)
